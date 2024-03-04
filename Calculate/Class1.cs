d7128381c1d24114802bf576699e72d57502059b
LukinAS <LukinAS@206D-2>
2024-03-04 14:11:19 +0700

    Добавьте файлы проекта.

---
+++ Calculate/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate
+{
+    public static class FunctionCalculator
+    {
+        public static void CalculateFunction(int N, int k, int iterations)
+        {
+            try
+            {
+                for (double x = 0; x <= k; x += 0.1)
+                {
+                    if (Math.Abs(x - N) < double.Epsilon)
+                    {
+                        throw new DivideByZeroException("Division by zero exception.");
+                    }
+                    if (x == double.PositiveInfinity || x == double.NegativeInfinity)
+                    {
+                        throw new ArgumentOutOfRangeException("Value out of range exception.");
+                    }
+                    double result = 1 / (x - N);
+                    Console.WriteLine($"f({x}) = {result}");
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Calculation finished.");
+            }
+        }
+    }
+}
---
+++ CalculateConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using Calculate;
+
+namespace Calculat
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N:");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число K:");
+            int k = int.Parse(Console.ReadLine());
+            int iterations = 10;
+            FunctionCalculator.CalculateFunction(N, k, iterations);
+            Console.ReadLine();
+        }
+    }
+}
